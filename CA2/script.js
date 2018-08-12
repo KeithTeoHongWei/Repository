a8e7ba2395b0f2e149a8886de3b6fcdc35f7541e
KeithTeoHongWei <38551367+KeithTeoHongWei@users.noreply.github.com>
2018-08-12 19:18:27 +0800

    update

--- CA2/script.js
+++ CA2/script.js
@@ -59,3 +59,19 @@ function carousel() {
     x[myIndex - 1].style.display = "block";
     setTimeout(carousel, 2500);
 }
+
+function readMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
