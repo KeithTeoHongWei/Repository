98516621a74839cfad52520ef551afaec101558f
KeithTeoHongWei <38551367+KeithTeoHongWei@users.noreply.github.com>
2018-08-12 15:50:35 +0800

    oh yea

--- CA2/script.js
+++ CA2/script.js
@@ -31,6 +31,36 @@ function scroll() {
     }
 }
 
+function menu() {
+    var x = document.querySelector("nav")
+    if (x.className === "sitenavigation") {
+        x.className += " navresponsive";
+    } else {
+        x.className = "sitenavigation";
+    }
+}
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
 var myIndex = 0;
 carousel();
 
@@ -44,13 +74,4 @@ function carousel() {
     if (myIndex > x.length) { myIndex = 1 }
     x[myIndex - 1].style.display = "block";
     setTimeout(carousel, 2500);
-}
-
-function menu() {
-    var x = document.querySelector("nav")
-    if (x.className === "sitenavigation") {
-        x.className += " navresponsive";
-    } else {
-        x.className = "sitenavigation";
-    }
 }
